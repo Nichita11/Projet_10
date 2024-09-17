e7376d696920c06764cfce8277832c7bd7703fd2
Nichita11 <153278056+Nichita11@users.noreply.github.com>
2024-09-17 18:38:42 +0200

    Add files via upload
    Realisation du projet 10

---
+++ src/containers/Slider/index.js
@@ -0,0 +1,64 @@
+import { Fragment, useEffect, useState } from "react";
+import { useData } from "../../contexts/DataContext";
+import { getMonth } from "../../helpers/Date";
+
+import "./style.scss";
+
+const Slider = () => {
+  const { data } = useData();
+  const [index, setIndex] = useState(0);
+  const byDateDesc = data?.focus?.length
+    ? data.focus.sort((evtA, evtB) =>
+        new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
+      )
+    : [];
+  useEffect(() => {
+    // const timeout = setTimeout(
+    //   () => setIndex(index < byDateDesc.length - 1 ? index + 1 : 0),
+    //   5000
+    // );
+    // return () => clearTimeout(timeout);
+    const interval = setInterval(
+      () => setIndex((prev) => (prev < byDateDesc.length - 1 ? prev + 1 : 0)),
+      5000
+    );
+    return () => clearInterval(interval);
+  }, [index]);
+  return (
+    <div className="SlideCardList">
+      {byDateDesc?.map((event, idx) => (
+        <Fragment key={Math.floor(Math.random() * 100000)}>
+          <div
+            className={`SlideCard SlideCard--${
+              index === idx ? "display" : "hide"
+            }`}
+          >
+            <img src={event.cover} alt="forum" />
+            <div className="SlideCard__descriptionContainer">
+              <div className="SlideCard__description">
+                <h3>{event.title}</h3>
+                <p>{event.description}</p>
+                <div>{getMonth(new Date(event.date))}</div>
+              </div>
+            </div>
+          </div>
+          <div className="SlideCard__paginationContainer">
+            <div className="SlideCard__pagination">
+              {byDateDesc.map((_, radioIdx) => (
+                <input
+                  key={Math.floor(Math.random() * 100000)}
+                  type="radio"
+                  name="radio-button"
+                  checked={index === radioIdx}
+                  onChange={() => setIndex(radioIdx)}
+                />
+              ))}
+            </div>
+          </div>
+        </Fragment>
+      ))}
+    </div>
+  );
+};
+
+export default Slider;
